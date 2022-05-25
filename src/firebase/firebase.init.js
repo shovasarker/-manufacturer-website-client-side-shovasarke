9187f3350f66726c1c87841ffdc1974ea3b2d1c3
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 15:49:13 +0600

    firebase.init file updated

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBesas0iad-cc0-sWjlTFBUVYRFPLVAOT8',
-  authDomain: 'abacus-parts.firebaseapp.com',
-  projectId: 'abacus-parts',
-  storageBucket: 'abacus-parts.appspot.com',
-  messagingSenderId: '450897691541',
-  appId: '1:450897691541:web:45afa75b6ff04955622c9c',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 // Initialize Firebase
