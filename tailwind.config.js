9657e1394f82f023fa3d4db043d38af997c9943a
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 14:23:28 +0600

    Neccessary Packages installation complete

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#02B290',
+          secondary: '#207398',
+          accent: '#1B98F5',
+          neutral: '#120E43',
+          'base-100': '#ffffff',
+          error: '#E21717',
+          success: '#38CC77',
+        },
+      },
+    ],
+  },
+}
