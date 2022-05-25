8504ba67bec79bc99596f82a1438a2c9806cdc96
ShovaSarker <shovasarker01@gmail.com>
2022-05-25 23:25:31 +0600

    SaerchResult Page Some Ui design Navigate back functionality added

--- src/pages/searchresult/SearchResult/index.jsx
+++ src/pages/searchresult/SearchResult/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+
 import PartsContainer from '../../shared/partscontainer/PartsContainer'
 import SearchBar from '../../shared/SearchBar'
+import Button from '../../standalone/Button'
 
 const SearchResult = () => {
   const { searchText } = useParams()
+  const navigate = useNavigate()
   return (
     <main>
+      <div className='container px-6'>
+        <Button
+          outlined
+          neutral
+          className={'my-5'}
+          onClick={() => navigate(-1)}
+        >
+          <AiOutlineArrowLeft className='w-5 h-5 mr-2' />
+          <>Go Back</>
+        </Button>
+      </div>
       <SearchBar />
+      <h2 className='text-2xl text-center my-10 text-neutral'>
+        Search Results for <span className='font-semibold'>"{searchText}"</span>
+      </h2>
       <PartsContainer
         url={`https://mwss-server.herokuapp.com/part/search?name=${searchText}`}
       />
